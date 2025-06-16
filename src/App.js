6759d8fa65c4996055d8efe008dcbe678da07f61
oscarrryeezus <oscamn2869@gmail.com>
2025-06-16 16:18:13 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './Layout';
+import { FaUserFriends, FaInfo } from 'react-icons/fa';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Bienvenidos a la Página de Inicio</h1>
+      <p style={{ fontSize: '18px', maxWidth: '600px', margin: '0 auto' }}>
+        Oscar Daniel Morales Navarro
+      </p>
+      <img
+        src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+        alt="Bienvenida"
+        style={{ width: '300px', marginTop: '20px' }}
+      />
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2><FaInfo /> Acerca de Nosotros</h2>
+      <p style={{ fontSize: '18px', maxWidth: '600px', margin: '0 auto' }}>
+        
+      </p>
+      <img
+        src="https://media.giphy.com/media/xUPGcguWZHRC2HyBRS/giphy.gif"
+        alt="Nosotros"
+        style={{ width: '280px', marginTop: '20px' }}
+      />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
